c4dc9e66a00eb45ec7cf21781dcc38c381bf784d
Pete Emerson <pete@gruntwork.io>
2023-03-16 20:17:53 -0400

    Block extraction with tests

--- lightbulb/lightbulb.go
+++ lightbulb/lightbulb.go
@@ -1,6 +1,7 @@
 package lightbulb
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"regexp"
@@ -20,7 +21,7 @@ type Block struct {
 	Set         string
 	ExitOnError bool
 	Keys        []string
-	Prompt      bool
+	Prompt      string
 	Secret      bool
 	Persist     bool
 	Sensitive   bool
@@ -28,122 +29,125 @@ type Block struct {
 
 // findAllBlocks finds all raw text blocks in a markdown string
 func findAllBlocks(content string) ([]string, error) {
-	log.Debug("Finding all blocks")
 	re := regexp.MustCompile("(?s)<!-- lightbulb:.*? -->.?```.*?```")
+	log.Debugf("Finding all blocks matching %s", re.String())
 	rawBlocks := re.FindAllString(content, -1)
-
 	return rawBlocks, nil
+}
 
+// getBlockAction returns the Lightbulb action from an HTML comment
+func getBlockAction(text string, block *Block) error {
+	re := regexp.MustCompile(`<!-- lightbulb:(\S+).*?-->`)
+	log.Debugf("Finding block action matching %s", re.String())
+	matches := re.FindStringSubmatch(text)
+	if len(matches) == 0 {
+		return errors.New("No matches in block found")
+	} else {
+		block.Action = matches[1]
+	}
+	return nil
+}
+
+func setBlockDefaults(block *Block) {
+	block.Tags = []string{"all"}
+	block.Mode = "0700"
+	block.Shell = "bash"
+	block.ExitOnError = true
+	block.Prompt = "missing"
+	block.Secret = false
+	block.Persist = true
+	block.Sensitive = false
+}
+
+// getBlockParameters returns the Lightbulb parameters from an HTML comment
+func getBlockParameters(text string, block *Block) error {
+	setBlockDefaults(block)
+	re := regexp.MustCompile(`<!-- lightbulb:\S+ (.*)-->`)
+	log.Debugf("Finding block parameters matching %s", re.String())
+	parameterMatches := re.FindStringSubmatch(text)
+	if len(parameterMatches) == 0 {
+		log.Infof("No parameters found in block")
+		return nil
+	}
+	parameters := parameterMatches[1]
+	parameters = strings.TrimSpace(parameters)
+	log.Debugf("Extracted parameters: %s", parameters)
+	re = regexp.MustCompile(`(\S+):(\S+)`)
+	log.Debugf("Finding parameters matching %s", re.String())
+	matches := re.FindAllStringSubmatch(parameters, -1)
+	for _, match := range matches {
+		key := match[1]
+		value := match[2]
+		switch key {
+		case "name":
+			block.Name = value
+		case "tags":
+			block.Tags = strings.Split(value, ",")
+		case "path":
+			block.Path = value
+		case "mode":
+			block.Mode = value
+		case "command":
+			block.Command = value
+		case "shell":
+			block.Shell = value
+		case "set":
+			block.Set = value
+		case "exit_on_error":
+			if value == "true" {
+				block.ExitOnError = true
+			}
+		case "keys":
+			block.Keys = strings.Split(value, ",")
+		case "prompt":
+			block.Prompt = value
+		case "secret":
+			if value == "true" {
+				block.Secret = true
+			}
+		case "persist":
+			if value == "true" {
+				block.Persist = true
+			}
+		case "sensitive":
+			if value == "true" {
+				block.Sensitive = true
+			}
+		default:
+			return fmt.Errorf("unknown parameter: %s", key)
+		}
+
+	}
+	return nil
 }
 
 // processTextBlock processes a raw text block into a Block
 func processTextBlock(text string) (Block, error) {
-	log.Debug("Processing text block")
+	log.Debugf("Processing text block:\n%s", text)
 	block := Block{}
-
-	re := regexp.MustCompile("<!-- lightbulb:(" + `\S+` + ").*? -->")
-	match := re.FindStringSubmatch(text)
-
-	lightbulbCommand := match[0]
-	block.Action = match[1]
-	log.Debugf("Block Action: %s", block.Action)
-
-	for _, param := range []string{"name", "tags", "path", "mode", "command", "shell", "set", "exitOnError", "keys", "prompt", "secret", "persist", "sensitive"} {
-		re = regexp.MustCompile(param + ":(" + `\S+` + ")")
-		match = re.FindStringSubmatch(lightbulbCommand)
-
-		// Required parameters
-		switch block.Action {
-		case "createFile":
-			if len(match) == 0 && (param == "name" || param == "path") {
-				return block, fmt.Errorf("no required parameter '%s' found in lightbulb '%s' action", param, block.Action)
-			}
-		case "runShell":
-			if len(match) == 0 && param == "name" {
-				return block, fmt.Errorf("no required parameter '%s' found in lightbulb '%s' action", param, block.Action)
-			}
-		case "setEnvironmentVars":
-			if len(match) == 0 && (param == "name" || param == "keys") {
-				return block, fmt.Errorf("no required parameter '%s' found in lightbulb '%s' action", param, block.Action)
-			}
-		}
-
-		if len(match) > 0 {
-			switch param {
-			case "name":
-				block.Name = match[1]
-			case "tags":
-				block.Tags = strings.Split(match[1], ",")
-			case "path":
-				block.Path = match[1]
-			case "mode":
-				block.Mode = match[1]
-			case "command":
-				block.Command = match[1]
-			case "shell":
-				block.Shell = match[1]
-			case "set":
-				block.Set = match[1]
-			case "exitOnError":
-				if match[1] == "true" {
-					block.ExitOnError = true
-				} else if match[1] == "false" {
-					block.ExitOnError = false
-				} else {
-					return block, fmt.Errorf("parameter '%s' found in lightbulb '%s' action is not true or false", param, block.Action)
-				}
-			case "keys":
-				block.Keys = strings.Split(match[1], ",")
-			case "prompt":
-				if match[1] == "true" {
-					block.Prompt = true
-				} else if match[1] == "false" {
-					block.Prompt = false
-				} else {
-					return block, fmt.Errorf("parameter '%s' found in lightbulb '%s' action is not true or false", param, block.Action)
-				}
-			case "secret":
-				if match[1] == "true" {
-					block.Secret = true
-				} else if match[1] == "false" {
-					block.Secret = false
-				} else {
-					return block, fmt.Errorf("parameter '%s' found in lightbulb '%s' action is not true or false", param, block.Action)
-				}
-			case "persist":
-				if match[1] == "true" {
-					block.Persist = true
-				} else if match[1] == "false" {
-					block.Persist = false
-				} else {
-					return block, fmt.Errorf("parameter '%s' found in lightbulb '%s' action is not true or false", param, block.Action)
-				}
-			case "sensitive":
-				if match[1] == "true" {
-					block.Sensitive = true
-				} else if match[1] == "false" {
-					block.Sensitive = false
-				} else {
-					return block, fmt.Errorf("parameter '%s' found in lightbulb '%s' action is not true or false", param, block.Action)
-				}
-
-			}
-		}
-
+	err := getBlockAction(text, &block)
+	if err != nil {
+		return block, err
 	}
-
+	err = getBlockParameters(text, &block)
+	if err != nil {
+		return block, err
+	}
 	return block, nil
 }
 
 // Parse parses a markdown string into a slice of Blocks
 func Parse(content string) ([]Block, error) {
+	log.Info("Parsing markdown")
+	log.Debugf("Markdown:\n%s", content)
+
 	rawBlocks, err := findAllBlocks(content)
 	if err != nil {
 		return nil, err
 	}
+	log.Debugf("Found %d blocks", len(rawBlocks))
 
-	var blocks []Block
-
+	blocks := []Block{}
 	for _, rawBlock := range rawBlocks {
 		block, err := processTextBlock(rawBlock)
 		if err != nil {
@@ -153,17 +157,17 @@ func Parse(content string) ([]Block, error) {
 	}
 
 	return blocks, nil
-
 }
 
 // LoadFromFile loads a markdown file into a string
 func LoadFromFile(fileName string) (string, error) {
-	log.Debugf("Loading markdown from file: %s", fileName)
+	log.Infof("Loading markdown from file: %s", fileName)
 
 	content, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return "", err
 	}
+	log.Debugf("File contents:\n%s", content)
 	return string(content), nil
 }
 
--- lightbulb/lightbulb_test.go
+++ lightbulb/lightbulb_test.go
@@ -1,8 +1,6 @@
 package lightbulb
 
-import (
-	"testing"
-)
+import "testing"
 
 func arrayMatch(left []string, right []string) bool {
 	if len(left) != len(right) {
@@ -14,6 +12,94 @@ func arrayMatch(left []string, right []string) bool {
 		}
 	}
 	return true
+}
+
+func TestFindAllblocks(t *testing.T) {
+	text, err := LoadFromFile("good.md")
+	if err != nil {
+		t.Error(err)
+	}
+	blocks, err := findAllBlocks(text)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(blocks) != 2 {
+		t.Error("Expected 2 blocks, got ", len(blocks))
+		return
+	}
+
+	sections := []string{
+		`<!-- lightbulb:createFile name:dateFile path:date.sh tags:one,two,three mode:0700 -->
+` + "```" + `shell
+#!/bin/bash
+
+echo "The current date in UTC is $(date -u)."
+` + "```",
+		`<!-- lightbulb:runShell name:runDate shell:bash -->
+` + "```" + `console
+./date.sh
+` + "```",
+	}
+
+	for i, section := range sections {
+		if blocks[i] != section {
+			t.Error("Block ", i, " doesn't match expected")
+		}
+	}
+}
+
+func TestGetBlockAction(t *testing.T) {
+	block := Block{}
+	text := `<!-- lightbulb:createFile name:dateFile path:date.sh tags:one,two,three mode:0700 -->`
+	err := getBlockAction(text, &block)
+	if err != nil {
+		t.Error(err)
+	}
+	if block.Action != "createFile" {
+		t.Error("Expected action createFile, got ", block.Action)
+	}
+	text = `<!-- lightbulb:runShell name:runDate shell:bash -->`
+	block = Block{}
+	err = getBlockAction(text, &block)
+	if err != nil {
+		t.Error(err)
+	}
+	if block.Action != "runShell" {
+		t.Error("Expected action runShell, got ", block.Action)
+	}
+	text = `<!-- lightbulb: name:runDate shell:bash -->`
+	block = Block{}
+	err = getBlockAction(text, &block)
+	if err == nil {
+		t.Error("Expected error, got ", block.Action)
+	}
+	if block.Action != "" {
+		t.Error("Expected action '', got ", block.Action)
+	}
+
+}
+
+func TestGetBlockParameters(t *testing.T) {
+	block := Block{}
+	text := `<!-- lightbulb:createFile name:dateFile path:date.sh tags:one,two,three mode:0700 -->`
+	err := getBlockParameters(text, &block)
+	if err != nil {
+		t.Error(err)
+	}
+	if block.Name != "dateFile" {
+		t.Error("Expected name dateFile, got ", block.Name)
+	}
+	if block.Path != "date.sh" {
+		t.Error("Expected path date.sh, got ", block.Path)
+	}
+	if !arrayMatch(block.Tags, []string{"one", "two", "three"}) {
+		t.Error("Expected tags [one two three], got ", block.Tags)
+	}
+	if block.Mode != "0700" {
+		t.Error("Expected mode 0700, got ", block.Mode)
+	}
+
 }
 
 func TestParse(t *testing.T) {
@@ -30,86 +116,110 @@ func TestParse(t *testing.T) {
 		t.Error("Expected 2 blocks, got ", len(blocks))
 		return
 	}
-
-	goodCompare := []Block{
-		{
-			Action:      "createFile",
-			Name:        "dateFile",
-			Tags:        []string{"one", "two", "three"},
-			Path:        "date.sh",
-			Mode:        "0700",
-			Command:     "",
-			Shell:       "",
-			Set:         "",
-			ExitOnError: false,
-			Keys:        []string{},
-			Prompt:      false,
-			Secret:      false,
-			Persist:     false,
-			Sensitive:   false,
-		},
-		{
-			Action:      "runShell",
-			Name:        "runDate",
-			Tags:        []string{},
-			Path:        "",
-			Mode:        "",
-			Command:     "",
-			Shell:       "bash",
-			Set:         "",
-			ExitOnError: false,
-			Keys:        []string{},
-			Prompt:      false,
-			Secret:      false,
-			Persist:     false,
-			Sensitive:   false,
-		},
+	if blocks[0].Action != "createFile" {
+		t.Error("Expected block.Action createFile, got ", blocks[0].Action)
 	}
-
-	for i, block := range goodCompare {
-		if block.Action != blocks[i].Action {
-			t.Error("Expected block.Action ", block.Action, " got ", blocks[i].Action)
-		}
-		if block.Name != blocks[i].Name {
-			t.Error("Expected block.Name ", block.Name, " got ", blocks[i].Name)
-		}
-		if !arrayMatch(block.Tags, blocks[i].Tags) {
-			t.Error("Expected block.Tags ", block.Tags, " got ", blocks[i].Tags)
-		}
-		if block.Path != blocks[i].Path {
-			t.Error("Expected block.Path ", block.Path, " got ", blocks[i].Path)
-		}
-		if block.Mode != blocks[i].Mode {
-			t.Error("Expected block.Mode ", block.Mode, " got ", blocks[i].Mode)
-		}
-		if block.Command != blocks[i].Command {
-			t.Error("Expected block.Command ", block.Command, " got ", blocks[i].Command)
-		}
-		if block.Shell != blocks[i].Shell {
-			t.Error("Expected block.Shell ", block.Shell, " got ", blocks[i].Shell)
-		}
-		if block.Set != blocks[i].Set {
-			t.Error("Expected block.Set ", block.Set, " got ", blocks[i].Set)
-		}
-		if block.Prompt != blocks[i].Prompt {
-			t.Error("Expected block.Prompt ", block.Prompt, " got ", blocks[i].Prompt)
-		}
-		if !arrayMatch(block.Keys, blocks[i].Keys) {
-			t.Error("Expected block.Keys ", block.Keys, " got ", blocks[i].Keys)
-		}
-		if block.Prompt != blocks[i].Prompt {
-			t.Error("Expected block.Prompt ", block.Prompt, " got ", blocks[i].Prompt)
-		}
-		if block.Secret != blocks[i].Secret {
-			t.Error("Expected block.Secret ", block.Secret, " got ", blocks[i].Secret)
-		}
-		if block.Persist != blocks[i].Persist {
-			t.Error("Expected block.Persist ", block.Persist, " got ", blocks[i].Persist)
-		}
-		if block.Sensitive != blocks[i].Sensitive {
-			t.Error("Expected block.Sensitive ", block.Sensitive, " got ", blocks[i].Sensitive)
-		}
-
+	if blocks[0].Name != "dateFile" {
+		t.Error("Expected block.Name dateFile, got ", blocks[0].Name)
+	}
+	if blocks[0].Path != "date.sh" {
+		t.Error("Expected block.Path date.sh, got ", blocks[0].Path)
+	}
+	if !arrayMatch(blocks[0].Tags, []string{"one", "two", "three"}) {
+		t.Error("Expected block.Tags [one two three], got ", blocks[0].Tags)
+	}
+	if blocks[0].Mode != "0700" {
+		t.Error("Expected block.Mode 0700, got ", blocks[0].Mode)
+	}
+	if blocks[1].Action != "runShell" {
+		t.Error("Expected block.Action runShell, got ", blocks[1].Action)
+	}
+	if blocks[1].Name != "runDate" {
+		t.Error("Expected block.Name runDate, got ", blocks[1].Name)
+	}
+	if blocks[1].Shell != "bash" {
+		t.Error("Expected block.Shell bash, got ", blocks[1].Shell)
 	}
 
 }
+
+// 	goodCompare := []Block{
+// 		{
+// 			Action:      "createFile",
+// 			Name:        "dateFile",
+// 			Tags:        []string{"one", "two", "three"},
+// 			Path:        "date.sh",
+// 			Mode:        "0700",
+// 			Command:     "",
+// 			Shell:       "",
+// 			Set:         "",
+// 			ExitOnError: false,
+// 			Keys:        []string{},
+// 			Prompt:      false,
+// 			Secret:      false,
+// 			Persist:     false,
+// 			Sensitive:   false,
+// 		},
+// 		{
+// 			Action:      "runShell",
+// 			Name:        "runDate",
+// 			Tags:        []string{},
+// 			Path:        "",
+// 			Mode:        "",
+// 			Command:     "",
+// 			Shell:       "bash",
+// 			Set:         "",
+// 			ExitOnError: false,
+// 			Keys:        []string{},
+// 			Prompt:      false,
+// 			Secret:      false,
+// 			Persist:     false,
+// 			Sensitive:   false,
+// 		},
+// 	}
+
+// 	for i, block := range goodCompare {
+// 		if block.Action != blocks[i].Action {
+// 			t.Error("Expected block.Action ", block.Action, " got ", blocks[i].Action)
+// 		}
+// 		if block.Name != blocks[i].Name {
+// 			t.Error("Expected block.Name ", block.Name, " got ", blocks[i].Name)
+// 		}
+// 		if !arrayMatch(block.Tags, blocks[i].Tags) {
+// 			t.Error("Expected block.Tags ", block.Tags, " got ", blocks[i].Tags)
+// 		}
+// 		if block.Path != blocks[i].Path {
+// 			t.Error("Expected block.Path ", block.Path, " got ", blocks[i].Path)
+// 		}
+// 		if block.Mode != blocks[i].Mode {
+// 			t.Error("Expected block.Mode ", block.Mode, " got ", blocks[i].Mode)
+// 		}
+// 		if block.Command != blocks[i].Command {
+// 			t.Error("Expected block.Command ", block.Command, " got ", blocks[i].Command)
+// 		}
+// 		if block.Shell != blocks[i].Shell {
+// 			t.Error("Expected block.Shell ", block.Shell, " got ", blocks[i].Shell)
+// 		}
+// 		if block.Set != blocks[i].Set {
+// 			t.Error("Expected block.Set ", block.Set, " got ", blocks[i].Set)
+// 		}
+// 		if block.Prompt != blocks[i].Prompt {
+// 			t.Error("Expected block.Prompt ", block.Prompt, " got ", blocks[i].Prompt)
+// 		}
+// 		if !arrayMatch(block.Keys, blocks[i].Keys) {
+// 			t.Error("Expected block.Keys ", block.Keys, " got ", blocks[i].Keys)
+// 		}
+// 		if block.Prompt != blocks[i].Prompt {
+// 			t.Error("Expected block.Prompt ", block.Prompt, " got ", blocks[i].Prompt)
+// 		}
+// 		if block.Secret != blocks[i].Secret {
+// 			t.Error("Expected block.Secret ", block.Secret, " got ", blocks[i].Secret)
+// 		}
+// 		if block.Persist != blocks[i].Persist {
+// 			t.Error("Expected block.Persist ", block.Persist, " got ", blocks[i].Persist)
+// 		}
+// 		if block.Sensitive != blocks[i].Sensitive {
+// 			t.Error("Expected block.Sensitive ", block.Sensitive, " got ", blocks[i].Sensitive)
+// 		}
+
+// 	}
